fe264d5a274937e944e0daaafa3396441fb732e5
unknown <erickcobiak@gmail.com>
2025-11-25 16:55:31 -0300

    Corrigido o botao sair

--- obj/Debug/net8.0/Sistema Veterinário.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sistema Veterinário.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sistema Veterinário")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6bbcc1424b25f1c279bd37f199e7bb7df5f9bc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sistema Veterinário")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sistema Veterinário")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
